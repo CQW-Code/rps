f8c5f966c9bdc59e8318a46aaf841d87fdea688b
Chris Watkin <chriswatkin@gmail.com>
2018-02-14 09:13:49 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,41 +1,53 @@
-function startGame(e) {
+var rpsArray = [ "rock", "paper", "scissors"]
+var index =  Math.floor(Math.random()* 3)
+
+function startGame(e)
+  {
     var choice = e.target.id
     console.log(choice)
+   
+    //var rpsArray = [ "rock", "paper", "scissors"]
+    //var index =  Math.floor(Math.random()* 3)
+    console.log(rpsArray[index])
+    var div = document.getElementById("computer")
+    div.innerHTML = rpsArray[index]
+    winOrLose(choice, index)
   }
-  
+ 
+
   var choices = document.getElementsByClassName('choice')
   for (var i in choices) {
     try {
-      choices[i].addEventListener('click', startGame)
-    } catch(err) {
-      //First load
-     
+      choices[i].addEventListener('click', startGame) 
     
-    }
-}
-//completely lost on this one.  The computer response does not even show
-//up in the console log and I'm not sure why.
+       } 
+    catch(err) 
+       {
+      //First load
+        }
+      
+      }
 
-function computerTurn()
-  { 
-   //var picked = e.target.id
-    var rpsArray = [ "rock", "paper", "scissors"]
-    var picked =  Math.floor(Math.random() * 3)
-    console.log(rpsArray[picked])
+var user = choices[i]
+var puter = rpsArray[index]
 
-    div.innerHTML = rpsArray[picked]
-  }
-  var picked = document.getElementsByClassName('computer')
-
-function winOrLose()
+function winOrLose(user, puter)
 { 
   //need to compare user choice with computer choice to see who wins
-  // rock wins over scissors
-  // paper wins over rock
-  // scissors win over paper
-  // tie
-}
+  //use if/else statement here...
+  // rock wins over scissors 0 --> 2
+  // paper wins over rock  2 --->  1
+  // scissors win over paper 2 ---> 1
+  // tie - done
+  
+  if (user[0] == puter[0])
+     if (user[1] == puter[1])
+       if (user[2] == puter[2])
+         console.log("A tie!")
+  else
+     console.log("who won?")
 
+}
   
   
   
